74dc70d78a01baf76e58200b7c62ec5745ec26a3
Vladimir Barrón <A16003754@alumnos.uady.mx>
2025-02-24 22:09:38 -0600

    feat: add register and login page in front

---
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './LoginPage.css';
+const LoginPage = () => {
+    //insert code here to create useState hook variables for email, password
+    const { form, setForm } = useState({
+        email : "",
+        password : ""
+    });
+
+    // insert code here to create handleLogin function and include console.log
+    const handleLogin = async (form) => {
+        console.log("Login successfull ", form);
+    }
+
+        return (
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6 col-lg-4">
+            <div className="login-card p-4 border rounded">
+              <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+                <form>
+                    <div className='mb-3'>
+                        <label htmlFor='email' className='form-label'>Email</label>
+                        <input
+                                        id='email' 
+                                        className='form-control' 
+                                        type='email' 
+                                        placeholder='Enter your email'
+                                        value={firstName}
+                                        onChange={(e) => setForm(v => ({...v, email:e.target.value}))}
+                                    />
+                        <label htmlFor='password' className=''>Password</label>
+                        <div className='mb-3'>
+                                    <label htmlFor='password' className='form label'>Password</label>
+                                    <input
+                                        id='password' 
+                                        className='form-control' 
+                                        type='password' 
+                                        placeholder='Enter your password'
+                                        value={firstName}
+                                        onChange={(e) => setForm(v => ({...v, password:e.target.value}))}
+                                    />
+                                </div>
+                    </div>
+                    <button className='btn btn-primary w-100 mb-3' type='submit' onClick={handleLogin(form)}>Login</button>
+
+                </form>
+                <p className="mt-4 text-center">
+                    New here? <a href="/app/register" className="text-primary">Register Here</a>
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+export default LoginPage;
---
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import './RegisterPage.css';
+
+const RegisterPage = () => {
+   const { form, setForm } = useState({ firstName : "", lastName : "", email : "", password : "" });
+
+   const handleRegister = async (form) => {
+        console.log("Register invoked", form);
+   }
+   
+    return (
+        <div className='container mt-5'>
+            <div className='row justify-content-center'>
+                <div className='col-md-6 col-lg-4'>
+                    <div className='register-card p-4 border rounded'>
+                        <h2 className='text-center mb-4 font-weight-bold '>Register</h2>
+                            <form>
+                                <div className='mb-4'>
+                                    <label htmlFor='firstName' className='form label'>First Name</label>
+                                    <input
+                                        id='firstName' 
+                                        className='form-control' 
+                                        type='text' 
+                                        placeholder='Enter your first name'
+                                        value={firstName}
+                                        onChange={(e) => setForm(v => ({...v, firstName:e.target.value}))}
+                                    />
+                                </div>
+                                <div className='mb-4'>
+                                    <label htmlFor='firstName' className='form label'>Last Name</label>
+                                    <input
+                                        id='lastName' 
+                                        className='form-control' 
+                                        type='text' 
+                                        placeholder='Enter your last name'
+                                        value={lastName}
+                                        onChange={(e) => setForm(v => ({...v, lastName:e.target.value}))}
+                                    />
+                                </div>
+                                <div className='mb-4'>
+                                    <label htmlFor='email' className='form label'>Email</label>
+                                    <input
+                                        id='email' 
+                                        className='form-control' 
+                                        type='email' 
+                                        placeholder='Enter your email'
+                                        value={firstName}
+                                        onChange={(e) => setForm(v => ({...v, email:e.target.value}))}
+                                    />
+                                </div>
+                                <div className='mb-4'>
+                                    <label htmlFor='password' className='form label'>Password</label>
+                                    <input
+                                        id='password' 
+                                        className='form-control' 
+                                        type='password' 
+                                        placeholder='Enter your password'
+                                        value={firstName}
+                                        onChange={(e) => setForm(v => ({...v, password:e.target.value}))}
+                                    />
+                                </div>
+                                
+                                <button className='btn btn-primary w-100 mb-3' type='submit' onClick={handleRegister(form)}>Register</button>
+                            </form>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterPage;
